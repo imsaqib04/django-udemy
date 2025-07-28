83fb81ac3a1443d55f9454152c1818ef9b2ace32
saqib <saqibansari2011@gmail.com>
2025-07-28 10:38:22 +0530

    django-2

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from watchlist_app.api.views import WatchListAV, WatchDetails, StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import WatchListAV, WatchDetails, StreamPlatformAV, StreamPlatformDetailAV,ReviewList,ReviewDetails
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -7,4 +7,7 @@ urlpatterns = [
 
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-details'),
+
+    path('review',ReviewList.as_view(),name='review-list'),
+    path('review/<int:pk>',ReviewDetails.as_view(),name='reviw-detail'),
 ]
--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
@@ -24,31 +24,15 @@ class WatchList(models.Model):
     def __str__(self):
         return self.title
     
+    ## create new model for reviews
+    
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    description = models.CharField(max_length=200,null=True)
+    watchlist = models.ForeignKey(WatchList,on_delete=models.CASCADE,related_name="reviews")
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # Create your models here.
-
-# class Movie(models.Model):
-#     name = models.CharField(max_length=50)
-#     description = models.CharField(max_length=200)
-#     active = models.BooleanField(default=True)
-
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return str(self.rating) + " - " + self.watchlist.title
